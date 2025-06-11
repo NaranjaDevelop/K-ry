fa097a004fde903b5bcdc6533ea28d87de20d46c
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2025-06-10 20:41:36 -0500

    fix: Adjust gap in grid layout of Details component for improved spacing

--- src/Pages/Details/Details.tsx
+++ src/Pages/Details/Details.tsx
@@ -18,7 +18,7 @@ const Details = () => {
           <PlaylistHeader />
           <MembersList />
           
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-0">
             <div className="lg:col-span-2 ">
               <SongsList />
             </div>
