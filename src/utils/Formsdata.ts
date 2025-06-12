af5494b9aee55138a73a1026cfe8002536691bb7
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2025-06-12 16:32:24 -0500

    fix: remove unused image assets and update Formsdata for dynamic icon rendering

--- src/utils/Formsdata.ts
+++ src/utils/Formsdata.ts
@@ -1,7 +1,9 @@
 import starface from '../assets/Q1/starface.png'; import sad from '../assets/Q1/sad02.png'; import calm from '../assets/Q1/relieved01.png'; import cool from '../assets/Q1/sunglasses.png';
 import angle from '../assets/Q2/angle.png'; import mute from '../assets/Q2/mute.png'; import singright from '../assets/Q2/singright.png'; 
 import sleeping from '../assets/Q3/sleeping.png'; import smile from '../assets/Q3/smile.png'; import tongue from '../assets/Q3/tongue-wink-right.png'; import mused from '../assets/Q3/unamused.png';
-
+import music from '../assets/Q4/music-note-01.png'; import shuffle from '../assets/Q4/shuffle.png';
+import book from '../assets/Q5/book bookmark-02.png'; import dum from '../assets/Q5/dumbbell-01.png'; import group from '../assets/Q5/user-group.png';
+import head from '../assets/Q6/headphone-mute.png'; import mutehead from '../assets/Q6/headphone-mute.png';;
 
 const dataform =  {
   "formulario": [
@@ -45,9 +47,9 @@ const dataform =  {
       "pregunta": "4. ¿Preferís música cantada o sin voz?",
       "asocia": ["instrumentalness", "speechiness"],
       "opciones": [
-        { "texto": "Siempre con voz", "instrumentalness": 0.0, "speechiness": 0.7 },
-        { "texto": "Me gusta cuando hay mezcla de voz e instrumental", "instrumentalness": 0.5, "speechiness": 0.4 },
-        { "texto": "Prefiero canciones sin letra (instrumentales)", "instrumentalness": 0.9, "speechiness": 0.1 }
+        { "texto": "Siempre con voz", "instrumentalness": 0.0, "speechiness": 0.7, "icon": singright },
+        { "texto": "Me gusta cuando hay mezcla de voz e instrumental", "instrumentalness": 0.5, "speechiness": 0.4, "icon": shuffle },
+        { "texto": "Prefiero canciones sin letra (instrumentales)", "instrumentalness": 0.9, "speechiness": 0.1, "icon": music }
       ]
     },
     {
@@ -56,10 +58,10 @@ const dataform =  {
       "pregunta": "5. ¿Para qué usás más la música?",
       "asocia": ["energy", "tempo", "track_genre"],
       "opciones": [
-        { "texto": "Para concentrarme o estudiar 📚", "energy": 0.2, "tempo": 70, "track_genre": "classical" },
-        { "texto": "Para relajarme o dormir 😴", "energy": 0.1, "tempo": 60, "track_genre": "ambient" },
-        { "texto": "Para hacer ejercicio 🏃‍♀️", "energy": 0.8, "tempo": 130, "track_genre": "electronic" },
-        { "texto": "Para pasar el rato con amigos 🎉", "energy": 0.7, "tempo": 120, "track_genre": "latin" }
+        { "texto": "Para concentrarme o estudiar 📚", "energy": 0.2, "tempo": 70, "track_genre": "classical", "icon": book },
+        { "texto": "Para relajarme o dormir 😴", "energy": 0.1, "tempo": 60, "track_genre": "ambient" , "icon": sleeping},
+        { "texto": "Para hacer ejercicio 🏃‍♀️", "energy": 0.8, "tempo": 130, "track_genre": "electronic", "icon": dum},
+        { "texto": "Para pasar el rato con amigos 🎉", "energy": 0.7, "tempo": 120, "track_genre": "latin", "icon": group }
       ]
     },
     {
@@ -67,9 +69,9 @@ const dataform =  {
       "pregunta": "7. ¿Te molesta que las canciones tengan lenguaje explícito (groserías, etc.)?",
       "asocia": ["explicit"],
       "opciones": [
-        { "texto": "Sí, prefiero evitarlas", "explicit": false },
-        { "texto": "No me importa", "explicit": true },
-        { "texto": "Depende de la canción", "explicit": true }
+        { "texto": "Sí, prefiero evitarlas", "explicit": false,  "icon": mutehead },
+        { "texto": "No me importa", "explicit": true , "icon": head },
+        { "texto": "Depende de la canción", "explicit": true, "icon": mused }
       ]
     }
   ]
